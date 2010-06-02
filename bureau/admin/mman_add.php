8c13fb7318e21b636bc77ee2bd2bf86bf5553ae3
Benjamin Sonntag <benjamin@alternc.org>
2010-06-02 18:42:44 +0000

    style trbtn ligne de bouton

--- bureau/admin/mman_add.php
+++ bureau/admin/mman_add.php
@@ -58,7 +58,7 @@ if (!$quota->cancreate("mailman")) {
 <tr><th><label for="pass"><?php __("List password (confirm)"); ?> </label></th><td>
 	<input type="password" class="int" id="pass2" name="pass2" value="<?php ehe($pass); ?>" size="20" maxlength="64" />
 	</td></tr>
-<tr><td colspan="2">
+<tr class="trbtn"><td colspan="2">
   <input type="submit" class="inb" name="submit" value="<?php __("Create the list."); ?>"/>
   <input type="button" class="inb" name="cancel" value="<?php __("Cancel"); ?>" onclick="document.location='mman_list.php'"/>
 </td></tr>
