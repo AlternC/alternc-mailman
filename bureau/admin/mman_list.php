2dedaa34e59b9e0a0932b3c79bf0415003b90162
Axel ROGER <axel.roger2@gmail.com>
2013-03-18 14:31:45 +0000

    Fixes #1474

--- bureau/admin/mman_list.php
+++ bureau/admin/mman_list.php
@@ -74,7 +74,7 @@ if ($quota->cancreate("mailman")) {
 
 	<form method="post" action="mman_edit.php">
 	<table class="tlist">
-	<tr><th><?php __("Delete"); ?></th><th><?php __("List name"); ?></th><th><?php __("List Status"); ?><th colspan="3">&nbsp;</th></tr>
+	<tr><th><?php __("Select"); ?></th><th><?php __("List name"); ?></th><th><?php __("List Status"); ?><th colspan="3">&nbsp;</th></tr>
 	<?php
 	reset($r);
 	$col=1;
